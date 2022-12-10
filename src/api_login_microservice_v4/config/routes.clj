a41035a649409e760101e6779d8eff12b9e36d22
Carlos Galaviz <carlos@email.com>
2022-12-10 01:07:49 -0700

    Added health check endpoint and remove pizza schema

--- src/api_login_microservice_v4/config/routes.clj
+++ src/api_login_microservice_v4/config/routes.clj
@@ -5,13 +5,6 @@
             [api-login-microservice-v4.config.swagger :as swagger-conf]))
 
 
-(s/defschema Pizza
-  {:name                         s/Str
-   (s/optional-key :description) s/Str
-   :size                         (s/enum :L :M :S)
-   :origin                       {:country (s/enum :FI :PO)
-                                  :city    s/Str}})
-
 (def document-routes
   (context "/api" []
            :tags ["api"]
@@ -22,11 +15,13 @@
                 :summary      "adds two numbers together"
                 (ok {:result (+ x y)}))
 
-           (POST "/echo" []
-                 :return  Pizza
-                 :body    [pizza Pizza]
-                 :summary "echoes a Pizza"
-                 (ok pizza))))
+           (GET "/health-check"
+                []
+                :return {:result String}
+                :summary "Health check services"
+                (ok {:result "Process is ok!"})
+             )
+           ))
 
 
 (def app-routes
---
+++ src/api_login_microservice_v4/entity/user.clj
@@ -0,0 +1,21 @@
+(ns api-login-microservice-v4.entity.user-entity
+  (:use [korma.core :only [defentity table entity-fields pk]])
+  (:require [clojure.tools.logging :as log]
+            [schema.core :as schema]))
+
+; -- Korma configuration
+(defentity user
+  (table :users) ; Associated table
+  (pk :id) ; primary key
+  (entity-fields :fisrt_name :last_name)) ; Default field for select
+
+; -- Validation schema for complete document
+(def user-schema
+  {:first_name schema/Str
+   :last_name schema/Str
+   :od schema/Int})
+
+; -- Validation for input rest document
+(def user-schema-rest-in
+  {:first_name schema/Str
+   :last_name schema/Str})
