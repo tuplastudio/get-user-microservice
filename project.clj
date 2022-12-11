de95c987e5ebfb595471e87b6e1d7701f978b4cf
Carlos Galaviz <carlos@email.com>
2022-12-11 11:21:34 -0700

    Added dependencies

--- project.clj
+++ project.clj
@@ -4,6 +4,11 @@
                   [compojure "1.7.0"] ; routing library
                   [org.clojure/tools.logging "1.2.4"] ; log library
                   [korma "0.5.0-RC1"] ; orm
+                  [org.clojure/java.jdbc "0.7.1"] ; jdbc support
+                  [cheshire "5.11.0"] ; Json parsing librairy
+                  [jarohen/nomad "0.9.1"] ; configuration handling library
+                  [prismatic/schema "1.4.1"] ; bean validation equivalent for clojure
+                  [ragtime "0.8.1"] ; migration utility
                   [metosin/compojure-api "2.0.0-alpha30"]]
    :ring {:handler api-login-microservice-v4.handler/app}
    :uberjar-name "server.jar"
